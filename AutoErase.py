63e40e18c433080cffba08d6ab798a20c6b1e441
Junhuan <Junhuanchen@qq.com>
2018-10-17 13:54:25 +0800

    ouput error and wait any key

--- AutoErase.py
+++ AutoErase.py
@@ -1,51 +1,54 @@
 # coding:utf-8
-import serial.tools.list_ports
+try:    
+    import serial.tools.list_ports
 
-print("Looking for upload port...")
-plist = list(serial.tools.list_ports.comports())
+    print("Looking for upload port...")
+    plist = list(serial.tools.list_ports.comports())
 
-serialName = ''
+    serialName = ''
 
-if len(plist) <= 0:
-    print("Serial Not Found!")
-else:
-    plist_0 = list(plist[len(plist) - 1])
-    serialName = plist_0[0]
-    print("Auto-detected:" + serialName)
+    if len(plist) <= 0:
+        print("Serial Not Found!")
+    else:
+        plist_0 = list(plist[len(plist) - 1])
+        serialName = plist_0[0]
+        print("Auto-detected:" + serialName)
 
-FLASH_MODE = "dio"
-FLASH_FREQ = "40m"
+    FLASH_MODE = "dio"
+    FLASH_FREQ = "40m"
 
-FLASH_START = "0x1000"
+    FLASH_START = "0x1000"
 
-from esptool import main 
+    from esptool import main 
 
-import sys
+    import sys
 
-bak = sys.argv
+    bak = sys.argv
 
-sys.argv = [
-	'AutoFlash.py', '--chip', 'esp32', 
-	'--port', serialName, 
-	'--baud', '460800', # 921600
-	'erase_flash'
-]
+    sys.argv = [
+            'AutoFlash.py', '--chip', 'esp32', 
+            '--port', serialName, 
+            '--baud', '460800', # 921600
+            'erase_flash'
+    ]
 
-try:
-    pos = bak.index('--port')
-    if pos is not False:
-        sys.argv[4] = bak[pos + 1]
-except ValueError:
-	pass
+    try:
+        pos = bak.index('--port')
+        if pos is not False:
+            sys.argv[4] = bak[pos + 1]
+    except ValueError:
+            pass
 
-try:
-    pos = bak.index('--baud')
-    if pos is not False:
-        sys.argv[6] = bak[pos + 1]
-except ValueError:
-	pass
+    try:
+        pos = bak.index('--baud')
+        if pos is not False:
+            sys.argv[6] = bak[pos + 1]
+    except ValueError:
+            pass
 
-main()
+    main()
+except Exception as e:
+    print(e)
 
 import os
 os.system("pause")
--- AutoFlashWebduino.py
+++ AutoFlashWebduino.py
@@ -34,7 +34,10 @@ try:
             '--flash_mode', FLASH_MODE, 
             '--flash_size', '4MB',
             '--flash_freq', FLASH_FREQ,
-            FLASH_START, 'firmware.bin'
+            '0x1000', 'bootloader_dio_40m.bin',
+            '0x8000', 'partitions.bin',
+            '0xe000', 'boot_app0.bin',
+            '0x10000', 'bit_default.bin',
     ]
 
     try:
